d1c58c76a8b77b5171bca9ef27a8f8fcd259ed31
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-10-18 14:09:45 +0200

    added basic JPA repository to the project

--- src/main/java/com/notification/reddert/repository/NotificationRepository.java
+++ src/main/java/com/notification/reddert/repository/NotificationRepository.java
@@ -1,4 +1,8 @@
 package com.notification.reddert.repository;
 
-public class NotificationRepository {
+import com.notification.reddert.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 }
