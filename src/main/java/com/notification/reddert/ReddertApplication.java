b571ffeba98c384692c662a943d65cf0902280c3
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-10-18 10:38:39 +0200

    inti

---
+++ src/main/java/com/notification/reddert/ReddertApplication.java
@@ -0,0 +1,13 @@
+package com.notification.reddert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReddertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notification/reddert/ReddertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.reddert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
