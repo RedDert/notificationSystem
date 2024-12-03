c5041600ae3f8bacef4f7fef2fc4087b02ac7c8a
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-12-03 11:20:21 +0100

    added OneToMany relation to the user class, JPA refactoring

--- src/main/java/com/reddert/notificationsystem/user/model/User.java
+++ src/main/java/com/reddert/notificationsystem/user/model/User.java
@@ -1,18 +1,24 @@
 package com.reddert.notificationsystem.user.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.reddert.notificationsystem.notification.model.Notification;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Table(name = "`user`")
 public class User {
+
     @Id
     @GeneratedValue
     private UUID id;
 
     private String name;
     private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Notification> notifications = new ArrayList<>();
 
     public User() {}
 
@@ -22,6 +28,7 @@ public class User {
     }
 
     // Getters and setters
+
     public UUID getId() {
         return id;
     }
@@ -44,5 +51,13 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<Notification> notifications) {
+        this.notifications = notifications;
     }
 }
