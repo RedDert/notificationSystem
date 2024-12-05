7afb356d0ffeba7d0a8d6e9077323caddd0a0796
DilanR <dilanredha@hotmail.com>
2024-12-05 22:27:40 +0100

    Expanded exceptions, TODO move in filesystem and add more exceptions.

--- src/main/java/com/reddert/notificationsystem/notification/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/reddert/notificationsystem/notification/exceptions/GlobalExceptionHandler.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,5 +22,17 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleGlobalException(Exception ex, WebRequest request) {
         return "An error occurred: " + ex.getMessage();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleEntityNotFoundException(Exception ex, WebRequest request) {
+        return "Entity not found: " + ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMethodArgumentTypeMismatchException(Exception ex, WebRequest request) {
+        return "Wrong argument: " + ex.getMessage();
     }
 }
