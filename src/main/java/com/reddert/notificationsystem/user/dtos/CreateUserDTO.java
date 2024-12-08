f39081e79a2461881fa5ad18da089cfe68c0fd18
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-12-08 15:26:57 +0100

    Refactored DTO setup with spring annotations NotBlank Email and Size to reduce boilerplate in the UserService

--- src/main/java/com/reddert/notificationsystem/user/dtos/CreateUserDTO.java
+++ src/main/java/com/reddert/notificationsystem/user/dtos/CreateUserDTO.java
@@ -1,3 +1,9 @@
 package com.reddert.notificationsystem.user.dtos;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record CreateUserDTO(String name, String email) {}
+public record CreateUserDTO(
+        @NotBlank @Size(max = 100) String name,
+        @NotBlank @Email String email
+) {}
