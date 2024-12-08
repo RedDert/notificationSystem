1ee0b376109aa7c4177ee01c446d8ea3d4dda1da
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-12-08 17:07:20 +0100

    refactored CreateUserDTO with regex pattern for name and messages for the other restrictions

--- src/main/java/com/reddert/notificationsystem/user/dtos/CreateUserDTO.java
+++ src/main/java/com/reddert/notificationsystem/user/dtos/CreateUserDTO.java
@@ -1,9 +1,16 @@
 package com.reddert.notificationsystem.user.dtos;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CreateUserDTO(
-        @NotBlank @Size(max = 100) String name,
-        @NotBlank @Email String email
+        @NotBlank(message = "Name is required")
+        @Size(max = 100, message = "Name must not exceed 100 characters")
+        @Pattern(regexp = "[A-Za-z\\s]+", message = "Name can only contain alphabetic characters and spaces")
+        String name,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email
 ) {}
